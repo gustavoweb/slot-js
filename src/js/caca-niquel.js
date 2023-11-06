ab04dc705d8d0b62d94b3eb7183cf093187d8840
gustavoweb <contato@gustavoweb.com>
2023-11-05 22:01:49 -0300

    primeiro commit

---
+++ src/js/caca-niquel.js
@@ -0,0 +1,88 @@
+// Defina os símbolos do caça-níquel
+const simbolos = ["cereja", "limao", "laranja", "morango", "uva"];
+
+// Obtenha referências aos elementos HTML
+const resultadoElement = document.getElementById("resultado");
+const botaoJogar = document.getElementById("jogar");
+
+// Adicione um evento de clique ao botão de jogar
+botaoJogar.addEventListener("click", jogar);
+
+// Variável para armazenar a pontuação inicial
+let pontuacao = 100;
+
+// Obtenha a referência à pontuação no HTML
+const pontuacaoElement = document.getElementById("valorPontuacao");
+
+// Função para girar o caça-níquel
+function girarCacaNiquel() {
+    const resultado = [];
+
+    for (let i = 0; i < 3; i++) {
+        const simboloAleatorio = simbolos[Math.floor(Math.random() * simbolos.length)];
+        resultado.push(simboloAleatorio);
+    }
+
+    return resultado;
+}
+
+// Função para atualizar o resultado e a pontuação
+function atualizarResultado() {
+    const simbolos = document.querySelectorAll('.simbolo');
+    const resultadoGirado = girarCacaNiquel();
+    //resultadoElement.textContent = `${resultadoGirado}`;
+
+    // Mapeie os resultados para as imagens correspondentes
+    const imagens = resultadoGirado.map((simbolo, index) => {
+        return `src/img/${simbolo}.jpg`;
+    });
+
+    // Atualize as imagens dos símbolos
+    simbolos.forEach((simbolo, index) => {
+        simbolo.src = imagens[index];
+    });
+
+    if (resultadoGirado[0] === resultadoGirado[1] && resultadoGirado[1] === resultadoGirado[2]) {
+        // Três frutas iguais - Ganhe 15 pontos
+        pontuacao += 15;
+        resultadoElement.textContent = `${resultadoGirado} + 15`;
+    } else if (resultadoGirado[0] === resultadoGirado[1] || resultadoGirado[1] === resultadoGirado[2] || resultadoGirado[0] === resultadoGirado[2]) {
+        // Duas frutas iguais - Ganhe 2 pontos
+        pontuacao += 2;
+        resultadoElement.textContent = `${resultadoGirado} + 2`;
+    } else {
+        // Três frutas diferentes - Perca 5 pontos
+        pontuacao -= 5;
+        resultadoElement.textContent = `${resultadoGirado} - 5`;
+    }
+
+    // Atualize e exiba a pontuação no HTML
+    pontuacaoElement.textContent = pontuacao;
+
+    if (pontuacao <= 0) {
+        resultadoElement.textContent += " - Você perdeu! Recomece.";
+        pontuacao = 100; // Reinicie a pontuação
+    }
+
+
+    setTimeout(() => {
+        // Restaure os placeholders após 500ms
+        simbolos.forEach((simbolo, index) => {
+            simbolo.src = "src/img/slot.jpg";
+        });
+
+        
+    }, 5000);
+}
+
+
+// Função para iniciar o jogo
+function jogar() {
+    botaoJogar.disabled = true; // Desabilite o botão durante a animação
+    resultadoElement.textContent = "Rodando...";
+
+    setTimeout(() => {
+        atualizarResultado();
+        botaoJogar.disabled = false; // Reabilite o botão após a animação
+    }, 500); // A animação dura 3 segundos
+}
